763c1bc2c2c9e87d8634ce1ca47b514746eaf6ea
kean <grebenyuk.alexander@gmail.com>
2020-02-13 06:54:16 -0500

    Initial commit

---
+++ Demo/HeraldDemo/HeraldDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeraldDemo
+//
+//  Created by Alexander Grebenyuk on 13.02.2020.
+//  Copyright © 2020 kean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
