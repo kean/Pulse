5daf99cf0b25b4af7994845edbd23db689394b95
kean <grebenyuk.alexander@gmail.com>
2020-02-13 08:11:12 -0500

    Add Logger.Message and a basic log function

--- Sources/Herald/Logger/Logger.swift
+++ Sources/Herald/Logger/Logger.swift
@@ -2,6 +2,10 @@
 //
 // Copyright (c) 2020 Alexander Grebenyuk (github.com/kean).
 
+import Foundation
+
+import os
+
 public final class Logger {
     public enum Level {
         /// Verbose, fine-grained events.
@@ -15,11 +19,61 @@ public final class Logger {
         case fatal
     }
 
-    public struct System {
+    public struct System: ExpressibleByStringLiteral {
+        public let rawValue: String
+        public init(stringLiteral value: String) { self.rawValue = value }
+        public init(_ value: String) { self.rawValue = value }
 
+        public static let `default` = System("default")
+    }
+
+    public struct Category: ExpressibleByStringLiteral {
+        public let rawValue: String
+        public init(stringLiteral value: String) { self.rawValue = value }
+        public init(_ value: String) { self.rawValue = value }
+
+        public static let `default` = Category("default")
     }
 
     public struct Message {
+        public let created: Date
+        public let level: Level
+        public let system: System
+        public let category: Category
+        public let session: UUID
+        public let text: String
+    }
 
+    /// Set `isEnabled` to `false` to disable all of the logging.
+    public var isEnabled = true
+
+    /// An id of the current log sesion.
+    public private(set) var logSessionId = UUID()
+
+    /// Starts a new log session.
+    public func startNewLogSession() {
+        logSessionId = UUID()
+    }
+
+    /// A default logger.
+    public static let `default` = Logger()
+
+    /// - parameter level: Log level, `.debug` by default.
+    /// - parameter system: System, `.default` by default.
+    /// - parameter category: Category, `.default` by default.
+    /// - parameter message: The actual message to log.
+    public func log(level: Level = .debug, system: System = .default, category: Category = .default, _ text: @autoclosure () -> String) {
+        guard isEnabled else { return }
+
+        let message = Message(
+            created: Date(),
+            level: level,
+            system: system,
+            category: category,
+            session: logSessionId,
+            text: text()
+        )
+
+        #warning("TODO: implement logging + saving logs, should I use os_log for this?")
     }
 }
