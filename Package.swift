597fe9e3215050bc3b7c8625de4c39c1887c245a
Moritz Lang <hi@slashmo.codes>
2020-05-05 16:30:47 +0200

    Add swift-log dependency 📦

--- Package.swift
+++ Package.swift
@@ -20,15 +20,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Pulse",
-            dependencies: []),
+            dependencies: ["Logging"]),
         .testTarget(
             name: "PulseTests",
-            dependencies: ["Pulse"]),
+            dependencies: ["Pulse", "Logging"]),
     ]
 )
