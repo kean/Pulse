e1b0e2816be69e36067ed337303be37c3ac56dd1
kean <grebenyuk.alexander@gmail.com>
2021-02-08 19:01:50 -0500

    Make dynamic

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Pulse",
+            type: .dynamic,
             targets: ["Pulse"]),
     ],
     dependencies: [
