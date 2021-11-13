58dcb54e42c451c75b02ee3c3020f919264232b7
kean <grebenyuk.alexander@gmail.com>
2021-11-13 11:24:52 -0500

    Remove unused Deprecated.swift

--- Sources/PulseCore/Deprecated.swift
+++
@@ -1,7 +0,0 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2020–2021 Alexander Grebenyuk (github.com/kean).
-
-import Foundation
-
-// @available(*, deprecated, message: "Renamed to stopPrefetching")
