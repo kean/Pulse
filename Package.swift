db539562ca35723d70288f0131f9d51e1bda171a
kean <grebenyuk.alexander@gmail.com>
2022-07-15 21:03:18 -0400

    Fix missing Package.swift file

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "Pulse",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "Pulse", targets: ["Pulse"]),
+        .library(name: "PulseCore", targets: ["PulseCore"]),
+        .library(name: "PulseUI", targets: ["PulseUI"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0")
+    ],
+    targets: [
+        .target(name: "PulseUI", dependencies: ["PulseCore"]),
+        .target(name: "Pulse", dependencies: [.product(name: "Logging", package: "swift-log"), "PulseCore"]),
+        .target(name: "PulseCore"),
+        .testTarget(name: "PulseTests", dependencies: ["Pulse"], resources: [.process("Resources")])
+    ]
+)
