5917757ca9b5ee09c59cf60b8cbfb9f7a5b8ade8
kean <grebenyuk.alexander@gmail.com>
2022-07-17 21:50:35 -0400

    Add macOS demo

---
+++ Demo/macOS/ContentView.swift
@@ -0,0 +1,19 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020–2021 Alexander Grebenyuk (github.com/kean).
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Your app here")
+            .padding()
+            .frame(width: 400, height: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
