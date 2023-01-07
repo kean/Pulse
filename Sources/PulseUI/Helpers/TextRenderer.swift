a69c828f63fef946534bee9b4c30ea308ff8e00d
kean <grebenyuk.alexander@gmail.com>
2023-01-06 21:44:07 -0500

    Remoev TextMake

--- Sources/PulseUI/Helpers/TextRenderer.swift
+++ Sources/PulseUI/Helpers/TextRenderer.swift
@@ -408,3 +408,23 @@ private let style = """
 }
 </style>
 """
+
+struct NetworkContent: OptionSet {
+    let rawValue: Int16
+    init(rawValue: Int16) { self.rawValue = rawValue }
+
+    static let header = NetworkContent(rawValue: 1 << 0)
+    static let requestComponents = NetworkContent(rawValue: 1 << 1)
+    static let requestQueryItems = NetworkContent(rawValue: 1 << 2)
+    static let errorDetails = NetworkContent(rawValue: 1 << 3)
+    static let originalRequestHeaders = NetworkContent(rawValue: 1 << 4)
+    static let currentRequestHeaders = NetworkContent(rawValue: 1 << 5)
+    static let requestOptions = NetworkContent(rawValue: 1 << 6)
+    static let requestBody = NetworkContent(rawValue: 1 << 7)
+    static let responseHeaders = NetworkContent(rawValue: 1 << 8)
+    static let responseBody = NetworkContent(rawValue: 1 << 9)
+
+    static let all: NetworkContent = [
+        header, requestComponents, requestQueryItems, errorDetails, originalRequestHeaders, currentRequestHeaders, requestOptions, requestBody, responseHeaders, responseBody
+    ]
+}
