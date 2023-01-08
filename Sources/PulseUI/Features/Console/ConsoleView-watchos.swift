fe4707f64fdedda843d8198e4b9d1117cb315675
kean <grebenyuk.alexander@gmail.com>
2023-01-07 19:34:14 -0500

    Reimplement filter toolbar

--- Sources/PulseUI/Features/Console/ConsoleView-watchos.swift
+++ Sources/PulseUI/Features/Console/ConsoleView-watchos.swift
@@ -11,29 +11,51 @@ import SwiftUI
 struct ConsoleView: View {
     @ObservedObject var viewModel: ConsoleViewModel
 
+    @State private var isSettingsPresented = false
+
     init(viewModel: ConsoleViewModel) {
         self.viewModel = viewModel
     }
 
     var body: some View {
         List {
-//            NavigationLink(destination: SettingsView(viewModel: .init(store: viewModel.store))) {
-//                Label("Settings", systemImage: "gearshape")
-//            }
-//            Button(action: { viewModel.isOnlyErrors.toggle() }) {
-//                Label("Show Errors", systemImage: viewModel.isOnlyErrors ? "exclamationmark.octagon.fill" : "exclamationmark.octagon")
-//            }
-//            .listRowBackground(viewModel.isOnlyErrors ? Color.blue.cornerRadius(8) : nil)
-//            Button(action: viewModel.toggleMode) {
-//                Label("Show Requests", systemImage: "paperplane")
-//            }
-//            .listRowBackground(viewModel.mode == .network ? Color.blue.cornerRadius(8) : nil)
-
+            let stack = HStack {
+                Button(action: viewModel.toggleMode) {
+                    Image(systemName: "paperplane").font(.title3)
+                }
+                .background(viewModel.mode == .network ? Rectangle().foregroundColor(.blue).cornerRadius(8) : nil)
+                Button(action: { viewModel.isOnlyErrors.toggle() }) {
+                    Image(systemName: "exclamationmark.octagon").font(.title3)
+                }
+                .background(viewModel.isOnlyErrors ? Rectangle().foregroundColor(.red).cornerRadius(8) : nil)
+                Button(action: { isSettingsPresented = true }) {
+                    Image(systemName: "gearshape").font(.title3)
+                }
+            }
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color.clear)
+            if #available(watchOS 8.0, *) {
+                stack
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+            } else {
+                stack
+            }
             ConsoleMessagesForEach(messages: viewModel.entities)
         }
         .navigationTitle("Console")
         .onAppear(perform: viewModel.onAppear)
         .onDisappear(perform: viewModel.onDisappear)
+        .sheet(isPresented: $isSettingsPresented) {
+            SettingsView(viewModel: .init(store: viewModel.store))
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Close") {
+                            isSettingsPresented = false
+                        }
+                    }
+                }
+        }
     }
 }
 
