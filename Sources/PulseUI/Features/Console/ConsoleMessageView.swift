7c5c7614681cd2e152a661d505a3eedcd12ec81b
kean <grebenyuk.alexander@gmail.com>
2023-01-08 16:01:12 -0500

    Use standard ConsoleMessageView on macOS

--- Sources/PulseUI/Features/Console/ConsoleMessageView.swift
+++ Sources/PulseUI/Features/Console/ConsoleMessageView.swift
@@ -7,10 +7,11 @@ import Pulse
 import CoreData
 import Combine
 
+#if os(watchOS) || os(tvOS) || os(macOS)
+
 struct ConsoleMessageView: View {
     let viewModel: ConsoleMessageViewModel
 
-#if os(watchOS) || os(tvOS)
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
             HStack {
@@ -18,10 +19,13 @@ struct ConsoleMessageView: View {
                     .font(fontTitle)
                     .foregroundColor(.secondary)
                 Spacer()
+#if os(macOS)
+            PinView(viewModel: viewModel.pinViewModel, font: fontTitle)
+#endif
                 let time = Text(viewModel.time)
                     .font(fontTitle)
                     .foregroundColor(.secondary)
-                if #available(tvOS 15, watchOS 8, *) {
+                if #available(tvOS 15, watchOS 8, macOS 12, *) {
                     time.monospacedDigit()
                 } else {
                     time
@@ -37,60 +41,12 @@ struct ConsoleMessageView: View {
 #if os(watchOS)
     private let fontTitle = Font.system(size: 14)
     private let fontBody = Font.system(size: 15)
+#elseif os(macOS)
+    private let fontTitle = Font.body
+    private let fontBody = Font.body
 #else
     private let fontTitle = Font.caption
     private let fontBody = Font.caption
-#endif
-#else
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            HStack(alignment: .firstTextBaseline) {
-                title
-                PinView(viewModel: viewModel.pinViewModel, font: fonts.title)
-                Spacer()
-            }
-            text.lineLimit(4)
-        }
-        .padding(.vertical, 4)
-    }
-
-    private var title: some View {
-        badge + (Text(viewModel.title)
-            .foregroundColor(.secondary)
-            .font(fonts.title))
-    }
-
-#warning("TODO: simplify this on tvos")
-    private var badge: Text {
-        var separator: Text {
-            return Text(" · ")
-                .font(fonts.title)
-                .foregroundColor(.secondary)
-        }
-        return Text(viewModel.level)
-            .font(fonts.title)
-            .foregroundColor(.secondary)
-        + separator
-    }
-
-    private var pin: some View {
-        Image(systemName: "pin")
-            .font(fonts.title)
-            .foregroundColor(.secondary)
-    }
-
-    private var text: some View {
-        Text(viewModel.text)
-            .font(fonts.body)
-            .foregroundColor(viewModel.textColor)
-    }
-
-    private struct Fonts {
-        let title: Font
-        let body: Font
-    }
-
-    private let fonts = Fonts(title: .body, body: .body)
 #endif
 }
 
@@ -104,3 +60,5 @@ struct ConsoleMessageView_Previews: PreviewProvider {
 }
 #endif
 
+
+#endif
