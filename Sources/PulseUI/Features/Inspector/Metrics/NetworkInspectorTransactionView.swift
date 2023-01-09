250bd965b241bdc4924484e54ff067666502c77e
kean <grebenyuk.alexander@gmail.com>
2023-01-08 23:12:19 -0500

    Fix build on macOS

--- Sources/PulseUI/Features/Inspector/Metrics/NetworkInspectorTransactionView.swift
+++ Sources/PulseUI/Features/Inspector/Metrics/NetworkInspectorTransactionView.swift
@@ -17,7 +17,7 @@ struct NetworkInspectorTransactionView: View {
             viewModel.timingViewModel.map(TimingView.init)
 #if os(iOS) || os(macOS)
             NavigationLink(destination: destintionTransactionDetails) {
-                if #available(iOS 15, tvOS 15, *), let size = viewModel.transferSizeViewModel {
+                if #available(iOS 15, tvOS 15, macOS 12, *), let size = viewModel.transferSizeViewModel {
                     transferSizeView(size: size)
                 } else {
                     Text("Transaction Details")
@@ -32,7 +32,7 @@ struct NetworkInspectorTransactionView: View {
         }
     }
 
-    @available(iOS 15, tvOS 15, watchOS 8, *)
+    @available(iOS 15, tvOS 15, watchOS 8, macOS 12, *)
     @ViewBuilder
     private func transferSizeView(size: NetworkInspectorTransferInfoViewModel) -> some View {
         HStack {
