0ee3b731596c89d35954b6077b488ab34caaee24
kean <grebenyuk.alexander@gmail.com>
2023-01-19 22:34:10 -0500

    Fix build on iOS

---
+++ Sources/PulseUI/Features/Search/Views/ConsoleSearchContextMenu.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+import CoreData
+import Pulse
+import Combine
+
+#if os(iOS)
+
+@available(iOS 15, *)
+struct ConsoleSearchContextMenu: View {
+    @ObservedObject var viewModel: ConsoleSearchViewModel
+
+    @State private var isShowingAsText = false
+    @State private var shareItems: ShareItems?
+    @State private var isShowingShareStore = false
+
+    var body: some View {
+        Menu {
+            Section {
+                Menu(content: { shareMenu }) {
+                    Label("Share", systemImage: "square.and.arrow.up")
+                }
+            }
+            Section {
+                Button(action: { isShowingAsText.toggle() }) {
+                    if isShowingAsText {
+                        Label("View as List", systemImage: "list.bullet.rectangle.portrait")
+                    } else {
+                        Label("View as Text", systemImage: "text.quote")
+                    }
+                }
+            }
+            Section {
+                StringSearchOptionsMenu(options: $viewModel.options)
+            }
+        } label: {
+            Image(systemName: "ellipsis.circle")
+                .font(.system(size: 20))
+                .foregroundColor(.accentColor)
+        }
+        .sheet(item: $shareItems, content: ShareView.init)
+        .sheet(isPresented: $isShowingAsText) {
+            NavigationView {
+                ConsoleTextView(entities: .init(viewModel.results.map(\.entity))) {
+                    isShowingAsText = false
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var shareMenu: some View {
+        Button(action: { share(as: .plainText) }) {
+            Label("Share as Text", systemImage: "square.and.arrow.up")
+        }
+        Button(action: { share(as: .html) }) {
+            Label("Share as HTML", systemImage: "square.and.arrow.up")
+        }
+    }
+
+    private func share(as output: ShareOutput) {
+        viewModel.prepareForSharing(as: output) { item in
+            shareItems = item
+        }
+    }
+}
+#endif
---
+++ Sources/PulseUI/Features/Search/Views/ConsoleSearchToolbar.swift
@@ -0,0 +1,44 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020–2023 Alexander Grebenyuk (github.com/kean).
+
+import SwiftUI
+import Pulse
+import CoreData
+import Combine
+
+#if os(iOS)
+
+@available(iOS 15, *)
+struct ConsoleSearchToolbar: View {
+    let title: String
+    var isSpinnerNeeded = false
+    @ObservedObject var viewModel: ConsoleViewModel
+    @State private var isShowingFilters = false
+
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            Text(title)
+                .foregroundColor(.secondary)
+                .font(.subheadline.weight(.medium))
+            if isSpinnerNeeded {
+                ProgressView()
+                    .padding(.leading, 8)
+            }
+            Spacer()
+            HStack(spacing: 14) {
+                ConsoleSearchContextMenu(viewModel: viewModel.searchViewModel)
+                ConsoleFiltersView(viewModel: viewModel, isShowingFilters: $isShowingFilters)
+            }
+        }
+        .buttonStyle(.plain)
+        .sheet(isPresented: $isShowingFilters) {
+            NavigationView {
+                ConsoleSearchCriteriaView(viewModel: viewModel.searchCriteriaViewModel)
+                    .inlineNavigationTitle("Filters")
+                    .navigationBarItems(trailing: Button("Done") { isShowingFilters = false })
+            }
+        }
+    }
+}
+#endif
