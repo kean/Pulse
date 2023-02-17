4cee0c376e4da1259419b2cc5186fbdffe36bb1d
kean <grebenyuk.alexander@gmail.com>
2023-02-17 09:12:29 -0500

    Take advantage of the updated regex syntax

--- Tests/PulseUITests/ConsoleTextViewModelTests.swift
+++ Tests/PulseUITests/ConsoleTextViewModelTests.swift
@@ -75,7 +75,7 @@ final class ConsoleTextViewModelTests: ConsoleTestCase {
 
         // THEN
         wait(for: [textInsertedExpectation], timeout: 2)
-        XCTAssertNotNil(string.wholeMatch(of: #/(.*?) · Pending · GET https:\/\/example.com\/api\n/#), string)
+        XCTAssertNotNil(string.wholeMatch(of: #/(.*?) · Pending · GET https://example.com/api\n/#), string)
 
         // GIVEN
         let textUpdatedEpectation = self.expectation(description: "textUpdatedxpectation")
@@ -87,7 +87,7 @@ final class ConsoleTextViewModelTests: ConsoleTestCase {
 
         // THEN
         wait(for: [textUpdatedEpectation], timeout: 2)
-        XCTAssertNotNil(string.wholeMatch(of: #/(.*?) · 200 OK · GET https:\/\/example.com\/api\n/#), string)
+        XCTAssertNotNil(string.wholeMatch(of: #/(.*?) · 200 OK · GET https://example.com/api\n/#), string)
     }
 
     func testDetailsShown() throws {
