2762142656914376c43acc79ac23025af08f9849
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2023-03-26 11:07:21 -0400

    Hide WelcomeView when you select a document

--- App/macOS/App.swift
+++ App/macOS/App.swift
@@ -2,18 +2,24 @@
 //
 // Copyright (c) 2020–2023 Alexander Grebenyuk (github.com/kean).
 
+import AppKit
 import SwiftUI
 
 @main
 struct App: SwiftUI.App {
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "WelcomeView") {
             WelcomeView()
         }
         .windowStyle(.hiddenTitleBar)
 
         WindowGroup {
             PulseDocumentViewer()
+                .onAppear {
+                    NSApp.windows
+                        .first { $0.identifier?.rawValue.contains("WelcomeView") ?? false }?
+                        .close()
+                }
         }
         .windowStyle(.hiddenTitleBar)
         .windowToolbarStyle(.unified(showsTitle: false))
