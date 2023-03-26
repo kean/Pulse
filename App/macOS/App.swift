aaaae20ce4cd8b85e41d079882de263da64d3ba2
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2023-03-26 10:32:05 -0400

    Add Pulse macOS app target

---
+++ App/macOS/App.swift
@@ -0,0 +1,14 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020–2023 Alexander Grebenyuk (github.com/kean).
+
+import SwiftUI
+
+@main
+struct App: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/macOS/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Pulse App macOS
+//
+//  Created by Alexander Grebenyuk on 3/26/23.
+//  Copyright © 2023 kean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
