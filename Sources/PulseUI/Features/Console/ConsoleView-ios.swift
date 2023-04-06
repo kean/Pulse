ee61b217a10b87442895316c7a6e9bc21f5e9c50
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2023-04-06 16:58:04 -0400

    Fix network mode not working

--- Sources/PulseUI/Features/Console/ConsoleView-ios.swift
+++ Sources/PulseUI/Features/Console/ConsoleView-ios.swift
@@ -157,6 +157,6 @@ struct ConsoleView_Previews: PreviewProvider {
 extension ConsoleView {
     /// Creates a view pre-configured to display only network requests
     public static func network(store: LoggerStore = .shared) -> ConsoleView {
-        ConsoleView(viewModel: .init(store: store, isOnlyNetwork: true))
+        ConsoleView(viewModel: .init(store: store, mode: .tasks, isOnlyNetwork: true))
     }
 }
