029b888da0f31baaec9d2a27a14fbb77f26d6d79
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2023-04-15 09:02:41 -0400

    Add sessions to search criteria view

---
+++ Sources/PulseUI/Features/Filters/Views/ConsoleSessionsPickerView.swift
@@ -0,0 +1,28 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020–2023 Alexander Grebenyuk (github.com/kean).
+
+import SwiftUI
+import Pulse
+
+struct ConsoleSessionsPickerView: View {
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \LoggerSessionEntity.createdAt, ascending: false)])
+    private var sessions: FetchedResults<LoggerSessionEntity>
+
+    @State var selection: Set<LoggerSessionEntity> = []
+
+    var body: some View {
+        ConsoleSearchListSelectionView(title: "Sessions", items: Array(sessions), selection: $selection, description: \.formattedDate, label: {
+            Text($0.formattedDate)
+        })
+    }
+}
+
+struct Previews_ConsoleSessionsPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            ConsoleSessionsPickerView()
+        }
+        .injectingEnvironment(.init(store: .mock))
+    }
+}
