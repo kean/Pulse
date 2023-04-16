6626f35ba29896fe1dc6368c38613222005bb6be
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2023-04-15 20:42:23 -0400

    Add SessionPickerView

---
+++ Sources/PulseUI/Features/Sessions/SessionPickerView.swift
@@ -0,0 +1,23 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020–2023 Alexander Grebenyuk (github.com/kean).
+
+import Foundation
+import Pulse
+import SwiftUI
+import CoreData
+import Combine
+
+#if os(iOS) || os(macOS)
+
+@available(macOS 13, *)
+struct SessionPickerView: View {
+    @Binding var selection: Set<UUID>
+
+    var body: some View {
+        SessionListView(selection: $selection, sharedSessions: .constant(nil))
+            .environment(\.editMode, .constant(.active))
+    }
+}
+
+#endif
