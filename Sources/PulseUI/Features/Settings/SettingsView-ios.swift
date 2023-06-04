6ab02954f17866dcedc43718eadee0606020de76
Mahmood Tahir <matsoft90@gmail.com>
2023-06-04 13:39:54 -0400

    Add option to display custom headers in the list view

--- Sources/PulseUI/Features/Settings/SettingsView-ios.swift
+++ Sources/PulseUI/Features/Settings/SettingsView-ios.swift
@@ -10,7 +10,7 @@ import UniformTypeIdentifiers
 
 public struct SettingsView: View {
     @ObservedObject var viewModel: SettingsViewModel
-
+    @State private var newHeaderName = ""
     @EnvironmentObject private var settings: UserSettings
 
     public init(store: LoggerStore = .shared) {
@@ -33,6 +33,28 @@ public struct SettingsView: View {
             if viewModel.isRemoteLoggingAvailable {
                 Section {
                     RemoteLoggerSettingsView(viewModel: .shared)
+                }
+            }
+
+            Section(header: Text("List headers"), footer: Text("These headers will be included in the list view")) {
+                ForEach(settings.displayHeaders, id: \.self) {
+                    Text($0)
+                }
+                .onDelete { indices in
+                    settings.displayHeaders.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Header", text: $newHeaderName)
+                    Button(action: {
+                        withAnimation {
+                            settings.displayHeaders.append(newHeaderName)
+                            newHeaderName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add header")
+                    }
+                    .disabled(newHeaderName.isEmpty)
                 }
             }
         }
--- Sources/PulseUI/Features/Settings/UserSettings.swift
+++ Sources/PulseUI/Features/Settings/UserSettings.swift
@@ -17,4 +17,29 @@ final class UserSettings: ObservableObject {
 
     @AppStorage("sharing-output")
     var sharingOutput: ShareStoreOutput = .store
+
+    @AppStorage("display-headers")
+    var displayHeaders: [String] = []
+}
+
+// MARK: - Array + RawREpresentable
+
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
 }
