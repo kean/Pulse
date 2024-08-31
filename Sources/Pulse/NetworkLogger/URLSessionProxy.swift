03285dbbc9462c9f6a4f5b4b15b5664bbb2cd854
kean <grebenyuk.alexander@gmail.com>
2024-08-31 13:57:00 -0400

    Cleanup

--- Sources/Pulse/NetworkLogger/URLSessionProxy.swift
+++ Sources/Pulse/NetworkLogger/URLSessionProxy.swift
@@ -4,183 +4,189 @@
 
 import Foundation
 
+final class NetworkLoggerURLSessionSwizzlerDelegate: URLSessionSwizzlerDelegate {
+
+    private let logger: NetworkLogger
+
+    init(logger: NetworkLogger = .init()) {
+        self.logger = logger
+    }
+
+    func swizzlerSessionDidCallResume(task: URLSessionTask) {
+        logger.logTaskCreated(task)
+    }
+
+    func swizzlerSessionDidComplete(task: URLSessionTask, error: (any Error)?) {
+        logger.logTask(task, didCompleteWithError: error)
+    }
+
+    func swizzlerSessionDidFinishCollectingMetrics(task: URLSessionTask, metrics: URLSessionTaskMetrics) {
+        logger.logTask(task, didFinishCollecting: metrics)
+    }
+
+    func swizzlerSessionDidReceiveData(dataTask: URLSessionDataTask, data: Data) {
+        logger.logDataTask(dataTask, didReceive: data)
+    }
+}
+
+
+protocol URLSessionSwizzlerDelegate: AnyObject {
+    func swizzlerSessionDidCallResume(task: URLSessionTask)
+    func swizzlerSessionDidComplete(task: URLSessionTask, error: (any Error)?)
+    func swizzlerSessionDidFinishCollectingMetrics(task: URLSessionTask, metrics: URLSessionTaskMetrics)
+    func swizzlerSessionDidReceiveData(dataTask: URLSessionDataTask, data: Data)
+}
+
+@MainActor
+public final class URLSessionProxy {
+    static var proxy: URLSessionProxy?
+
+    let logger: NetworkLogger
+
+    init(logger: NetworkLogger) {
+        self.logger = logger
+    }
+
+    /// Enables automatic `URLSession` logging.
+    ///
+    /// - parameter logger: The network logger to be used for recording the requests.
+    public static func enable(with logger: NetworkLogger = .init()) {
+        guard URLSessionProxy.proxy == nil else {
+            NSLog("Pulse.URLSessionProxy already enabled")
+            return
+        }
+        let proxy = URLSessionProxy(logger: logger)
+        proxy.enable()
+        URLSessionProxy.proxy = proxy
+    }
+
+    func enable() {
+        injectIntoNSURLSessionTaskResume()
+        if let sessionClass = NSClassFromString("__NSCFURLLocalSessionConnection") {
+            injectIntoURLSessionDelegate(anyClass: sessionClass)
+        }
+    }
+
+    private func injectIntoURLSessionDelegate(anyClass: AnyClass) {
+        swizzleDataTaskDidReceiveData(baseClass: anyClass)
+        swizzleDataDataDidCompleteWithError(baseClass: anyClass)
+    }
+
+    private func injectIntoNSURLSessionTaskResume() {
+        var methodsToSwizzle = [Method]()
+
+        if let method = class_getInstanceMethod(URLSessionTask.self, #selector(URLSessionTask.resume)) {
+            methodsToSwizzle.append(method)
+        }
+
+        if let cfURLSession = NSClassFromString("__NSCFURLSessionTask"),
+           let method = class_getInstanceMethod(cfURLSession, NSSelectorFromString("resume")) {
+            methodsToSwizzle.append(method)
+        }
+
+        methodsToSwizzle.forEach {
+            let method = $0
+
+            var originalImplementation: IMP?
+            let block: @convention(block) (URLSessionTask) -> Void = { [weak self] task in
+                self?.logger.logTaskCreated(task)
+                guard task.currentRequest != nil else { return }
+                let key = String(method.hashValue)
+                objc_setAssociatedObject(task, key, true, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                let castedIMP = unsafeBitCast(originalImplementation, to: (@convention(c) (Any) -> Void).self)
+                castedIMP(task)
+                objc_setAssociatedObject(task, key, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+            let swizzledIMP = imp_implementationWithBlock(unsafeBitCast(block, to: AnyObject.self))
+            originalImplementation = method_setImplementation(method, swizzledIMP)
+        }
+    }
+}
+
+extension URLSessionProxy {
+
+    /// Swizzles the folowing methods:
+    ///
+    /// - urlSession(_:task:didCompleteWithError:)
+    func swizzleDataDataDidCompleteWithError(baseClass: AnyClass) {
+        let selector = NSSelectorFromString("_didFinishWithError:")
+        guard let method = class_getInstanceMethod(baseClass, selector),
+            baseClass.instancesRespond(to: selector) else {
+            return
+        }
+        typealias MethodSignature = @convention(c) (AnyObject, Selector, AnyObject?) -> Void
+        let originalImp: IMP = method_getImplementation(method)
+        let closure: @convention(block) (AnyObject, AnyObject?) -> Void = { [weak self] object, error in
+            let original: MethodSignature = unsafeBitCast(originalImp, to: MethodSignature.self)
+            original(object, selector, error)
+
+            if let task = object.value(forKey: "task") as? URLSessionTask {
+                if let metrics = task.value(forKey: "_incompleteTaskMetrics") as? URLSessionTaskMetrics {
+                    #warning("FIX")
+                    self?.logger.logTask(task, didFinishCollecting: metrics)
+                }
+                let error = error as? Error
+                self?.logger.logTask(task, didCompleteWithError: error)
+            } else {
+                NSLog("Could not get data from _swizzleURLSessionTaskDidCompleteWithError. It might causes due to the latest iOS changes. \(object)")
+            }
+        }
+
+        method_setImplementation(method, imp_implementationWithBlock(closure))
+    }
+
+    /// Swizzles the folowing methods:
+    ///
+    /// urlSession(_:dataTask:didReceive:)
+    func swizzleDataTaskDidReceiveData(baseClass: AnyClass) {
+        let selector = NSSelectorFromString("_didReceiveData:")
+        guard let method = class_getInstanceMethod(baseClass, selector),
+            baseClass.instancesRespond(to: selector) else {
+            return
+        }
+
+        typealias MethodSignature =  @convention(c) (AnyObject, Selector, AnyObject) -> Void
+        let originalImp: IMP = method_getImplementation(method)
+        let closure: @convention(block) (AnyObject, AnyObject) -> Void = { [weak self] (object, data) in
+            let original: MethodSignature = unsafeBitCast(originalImp, to: MethodSignature.self)
+            original(object, selector, data)
+
+            if let task = object.value(forKey: "task") as? URLSessionDataTask {
+                if let data = data as? Data {
+                    self?.logger.logDataTask(task, didReceive: data)
+                } else {
+                    // TODO: what should it do?
+                }
+            } else {
+                // TODO: update who to call
+                NSLog("Could not get data from _swizzleURLSessionDataTaskDidReceiveData. It might causes due to the latest iOS changes. \(object)")
+            }
+        }
+        method_setImplementation(method, imp_implementationWithBlock(closure))
+    }
+}
+
+@available(*, deprecated, message: "Experimental.URLSessionProxy is replaced with a reworked URLSessionProxy")
 public enum Experimental {
 }
 
+@available(*, deprecated, message: "Experimental.URLSessionProxy is replaced with a reworked URLSessionProxy")
 public extension Experimental {
-    /// WARNING: Experimental.
+    @MainActor
     final class URLSessionProxy {
-#if compiler(>=6.0)
-        public nonisolated(unsafe) static let shared = URLSessionProxy()
-#else
         public static let shared = URLSessionProxy()
-#endif
-
-        private init() {
-            session = URLSession(configuration: .default, delegate: URLSessionProxyDelegate(logger: logger, delegate: CustomProtocolSessionDelegate.shared), delegateQueue: CustomProtocolSessionDelegate.shared.queue)
-        }
-
-        /// Network logger to be used for storing the requests. By default,
-        /// uses the logger with the `.default` store.
-        public var logger: NetworkLogger = .init() {
-            didSet { resetSession() }
-        }
-
-        /// By default, `.default`.
-        public var configuration: URLSessionConfiguration = .default {
-            didSet { resetSession() }
-        }
-
-        /// By default, empty.
+        public var logger: NetworkLogger = .init()
+        public var configuration: URLSessionConfiguration = .default
         public var ignoredHosts = Set<String>()
 
         public var isEnabled: Bool = false {
             didSet {
                 if isEnabled {
-                    URLProtocol.registerClass(CustomHTTPProtocol.self)
+                    Pulse.URLSessionProxy.enable(with: logger)
                 } else {
-                    URLProtocol.unregisterClass(CustomHTTPProtocol.self)
+                    NSLog("Pulse.URLSessionProxy can't be disabled at runtime")
                 }
             }
         }
-
-        // MARK: URLSession
-
-        fileprivate var session: URLSession
-
-        private func resetSession() {
-            session.invalidateAndCancel()
-            configuration.protocolClasses?.insert(CustomHTTPProtocol.self, at: 0)
-            session = URLSession(configuration: configuration, delegate: URLSessionProxyDelegate(logger: logger, delegate: CustomProtocolSessionDelegate.shared), delegateQueue: CustomProtocolSessionDelegate.shared.queue)
-        }
-    }
-}
-
-private final class CustomHTTPProtocol: URLProtocol {
-    struct Constants {
-        static let RequestHandledKey = "URLProtocolRequestHandled"
-    }
-
-    var sessionTask: URLSessionDataTask?
-
-    override init(request: URLRequest, cachedResponse: CachedURLResponse?, client: URLProtocolClient?) {
-        super.init(request: request, cachedResponse: cachedResponse, client: client)
-    }
-
-    override class func canInit(with request: URLRequest) -> Bool {
-        guard CustomHTTPProtocol.shouldHandleRequest(request) else {
-            return false
-        }
-        if CustomHTTPProtocol.property(forKey: Constants.RequestHandledKey, in: request) != nil {
-            return false
-        }
-        return true
-    }
-
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-
-    override func startLoading() {
-        guard let newRequest = ((request as NSURLRequest).mutableCopy() as? NSMutableURLRequest) else {
-            return assertionFailure()
-        }
-        CustomHTTPProtocol.setProperty(true, forKey: Constants.RequestHandledKey, in: newRequest)
-        let dataTask = Experimental.URLSessionProxy.shared.session.dataTask(with: newRequest as URLRequest)
-        let handler = RequestHandler(proto: self)
-        CustomProtocolSessionDelegate.shared.register(handler, for: dataTask)
-        dataTask.resume()
-
-        DispatchQueue.main.async {
-            self.sessionTask = dataTask
-        }
-    }
-
-    override func stopLoading() {
-        sessionTask?.cancel()
-    }
-
-    private func body(from request: URLRequest) -> Data? {
-        /// The receiver will have either an HTTP body or an HTTP body stream only one may be set for a request.
-        /// A HTTP body stream is preserved when copying an NSURLRequest object,
-        /// but is lost when a request is archived using the NSCoding protocol.
-        return request.httpBody // ?? request.httpBodyStream?.readfully()
-    }
-
-    /// Inspects the request to see if the host has not been blacklisted and can be handled by this URL protocol.
-    /// - Parameter request: The request being processed.
-    private class func shouldHandleRequest(_ request: URLRequest) -> Bool {
-        guard let host = request.url?.host else {
-            return true
-        }
-        return !Experimental.URLSessionProxy.shared.ignoredHosts.contains(where: host.hasSuffix)
-    }
-
-    deinit {
-        sessionTask = nil
-    }
-
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        client?.urlProtocol(self, didLoad: data)
-    }
-
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-        let policy = URLCache.StoragePolicy(rawValue: request.cachePolicy.rawValue) ?? .notAllowed
-        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: policy)
-        completionHandler(.allow)
-    }
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            client?.urlProtocol(self, didFailWithError: error)
-        } else {
-            client?.urlProtocolDidFinishLoading(self)
-        }
-    }
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
-        client?.urlProtocol(self, wasRedirectedTo: request, redirectResponse: response)
-        completionHandler(request)
-    }
-}
-
-private final class CustomProtocolSessionDelegate: NSObject, URLSessionTaskDelegate, URLSessionDataDelegate {
-    static let shared = CustomProtocolSessionDelegate()
-
-    let queue = OperationQueue()
-    var handlers: [URLSessionTask: RequestHandler] = [:]
-
-    override init() {
-        queue.maxConcurrentOperationCount = 1
-    }
-
-    func register(_ handler: RequestHandler, for task: URLSessionTask) {
-        queue.addOperation { // `URLSession` is configured to use this same queue
-            self.handlers[task] = handler
-        }
-    }
-
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        handlers[dataTask]?.proto.urlSession(session, dataTask: dataTask, didReceive: data)
-    }
-
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-        handlers[dataTask]?.proto.urlSession(session, dataTask: dataTask, didReceive: response, completionHandler: completionHandler)
-    }
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        handlers[task]?.proto.urlSession(session, task: task, didCompleteWithError: error)
-    }
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
-        handlers[task]?.proto.urlSession(session, task: task, willPerformHTTPRedirection: response, newRequest: request, completionHandler: completionHandler)
-    }
-}
-
-private final class RequestHandler {
-    let proto: CustomHTTPProtocol
-
-    init(proto: CustomHTTPProtocol) {
-        self.proto = proto
     }
 }
